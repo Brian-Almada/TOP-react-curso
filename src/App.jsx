fd0d2e6724a5eba11c9d4694458df9c7b3a90a68
Brian Almada <brianalexisalmada84@gmail.com>
2023-08-21 22:08:25 -0300

    Usar props

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function ListItem(props) {
+  return <li>{props.animal}</li>
+}
+
+function List(props) {
+  return (
+    <ul>
+      {props.animals.map((animal) => {
+        return <ListItem key= {animal} animal = {animal} />;
+      })}
+    </ul>
+  );
+}
+
 function App() {
-  const animals = ["Lion", "Tiger", "Snake", "Lizard"];
+  const animals = ["Lion", "Tiger", "Snake", "Lizard", "Monkey"];
   return (
     <>
       <h1>Animals: </h1>
-      <ul>
-        {animals.map((animal) => {
-          return <li key={animal}>{animal}</li>;
-        })}
-      </ul>
+      <List animals={animals}/>
     </>
   )
 }
